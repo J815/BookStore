5bb969fddfbd9c7f3f1ee5ad3620aac1f33e4a14
jubayer <jubayermd327@gmail.com>
2023-06-24 15:06:20 +0600

    add books to a shopping cart

--- bookstore/frontend/src/components/BookList.js
+++ bookstore/frontend/src/components/BookList.js
@@ -1,21 +1,39 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export const BookList = () => {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
-  const fetchBooks = async () => {
+  // const [books, setBooks] = useState([]);
+  const handleAddToCart = async (bookId) => {
     try {
-      const booksData = await axios.get('/api/books/');
-      setBooks(booksData.data);
+      const response = await axios.post('http://localhost:8000/api/add-to-cart/', {
+        book_id: bookId,
+        quantity: 1,
+      });
+      console.log(response.data);
     } catch (error) {
-      console.error('Error fetching books:', error);
+      console.error(error);
     }
-  };
+    };
+
+  // useEffect(() => {
+  //   fetchBooks();
+  // }, []);
+
+  // const fetchBooks = async () => {
+  //   try {
+  //     const booksData = await axios.get('/api/books/');
+  //     setBooks(booksData.data);
+  //   } catch (error) {
+  //     console.error('Error fetching books:', error);
+  //   }
+  // };
+   const books=[
+    {id:1 ,title: 'b1', author: 'a1', price: 10},
+    {id:2 ,title: 'b2', author: 'a2', price: 100},
+    {id:12 ,title: 'b3', author: 'a1', price: 110},
+    {id:15 ,title: 'b16', author: 'a15', price: 130},
+   ];
+
   return (
         <div>
             <ul style={{ display: 'flex'}}>
@@ -26,6 +44,7 @@ export const BookList = () => {
                         <div> {book.title} </div>
                          <div>{book.author}</div>
                          <div>{book.price}</div>
+                         <button onClick={() => handleAddToCart(book.id)}>Add to Cart</button>
                     </div>
                 ))}
             </ul>
